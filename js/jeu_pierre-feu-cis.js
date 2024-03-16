fd5a3ea2b3c553dff3ee18917a09f461abac21da
Alex <alexandre.pitarch@divcom.ch>
2024-03-16 19:08:07 +0100

    favicon

--- js/jeu_pierre-feu-cis.js
+++ js/jeu_pierre-feu-cis.js
@@ -7,13 +7,15 @@
 
 'use strict'; // Active le mode strict du JavaScript
 
+//Prend les différent bouton de l'utilisateur
 const pierre = document.getElementById('pierre');
 const feuille = document.getElementById('feuille');
 const ciseaux = document.getElementById('ciseaux');
+
+//Variable qui contiendra le signe que l'IA fera à chaque tour
 let jeuOrdi;
 
 let signeImg = document.getElementById('signeImg');
-let signeOrdi = document.getElementById('signeOrdi');
 let resultat = document.getElementById('vainqueur');
 
 let scoreUtilisateur = document.getElementById('scoreJoueur1');
@@ -22,7 +24,7 @@ let scoreOrdi = document.getElementById('scoreBot');
 let compteurScoreUti = 0;
 let compteurScoreBot = 0;
 
-
+//tableau qui contient les 3 signes de l'ordinateur
 const tabOrdi = [
     { choix: 'pierre', image: 'img/pierre.png' },
     { choix: 'feuille', image: 'img/feuille.png' },
@@ -44,55 +46,55 @@ ciseaux.addEventListener('click', function () {
 function jouer(signeUtilisateur){
 
     let jeuOrdiIndex;
+    //génération automatique et aléatoire du signe de l'ordinateur
+    jeuOrdiIndex = Math.floor(Math.random() * tabOrdi.length);
+    jeuOrdi = tabOrdi[jeuOrdiIndex];
+    //signeOrdi.innerText = jeuOrdi.choix;
+    signeImg.src = jeuOrdi.image;
 
-            jeuOrdiIndex = Math.floor(Math.random() * tabOrdi.length);
-            jeuOrdi = tabOrdi[jeuOrdiIndex];
-            //signeOrdi.innerText = jeuOrdi.choix;
-            signeImg.src = jeuOrdi.image;
+    console.log(signeUtilisateur)
+    console.log(jeuOrdi.choix)
 
-            console.log(signeUtilisateur)
-            console.log(jeuOrdi.choix)
+    //Conditions qui permettent de voir si l'utilisateur gagne, perd ou si c'est un égalité
+    if (signeUtilisateur === 'pierre' && jeuOrdi.choix === 'ciseaux') {
+        resultat.innerText = 'Gagné!';
+        compteurScoreUti++;
+    } else if (signeUtilisateur === 'feuille' && jeuOrdi.choix === 'pierre') {
+        resultat.innerText = 'Gagné!';
+        compteurScoreUti++;
+    } else if (signeUtilisateur === 'ciseaux' && jeuOrdi.choix === 'feuille') {
+        resultat.innerText = 'Gagné!';
+        compteurScoreUti++;
+    } else if (signeUtilisateur === jeuOrdi.choix) {
+        resultat.innerText = 'Egalité!';
+    } else {
+        resultat.innerText = 'Perdu!';
+        compteurScoreBot++;               }
 
-                if (signeUtilisateur === 'pierre' && jeuOrdi.choix === 'ciseaux') {
-                    resultat.innerText = 'Gagné!';
-                    compteurScoreUti++;
-                } else if (signeUtilisateur === 'feuille' && jeuOrdi.choix === 'pierre') {
-                    resultat.innerText = 'Gagné!';
-                    compteurScoreUti++;
-                } else if (signeUtilisateur === 'ciseaux' && jeuOrdi.choix === 'feuille') {
-                    resultat.innerText = 'Gagné!';
-                    compteurScoreUti++;
-                } else if (signeUtilisateur === jeuOrdi.choix) {
-                    resultat.innerText = 'Egalité!';
-                } else {
-                    resultat.innerText = 'Perdu!';
-                    compteurScoreBot++;               }
+    //converti le score en string
+    compteurScoreUti = String(compteurScoreUti);
+    compteurScoreBot = String(compteurScoreBot);
 
-                compteurScoreUti = String(compteurScoreUti);
-                compteurScoreBot = String(compteurScoreBot);
+    scoreUtilisateur.innerText = compteurScoreUti;
+    scoreOrdi.innerText = compteurScoreBot
 
-                scoreUtilisateur.innerText = compteurScoreUti;
-                scoreOrdi.innerText = compteurScoreBot
-
-        if (compteurScoreUti >= 10){
-            alert('Félicitations!\n' +
-                'Vous avez gagné! 😎 ');
-            compteurScoreUti = 0;
-            compteurScoreBot = 0;
-            scoreUtilisateur.innerText = compteurScoreUti;
-            scoreOrdi.innerText = compteurScoreBot
-        }
-        else if (compteurScoreBot >= 10){
-            alert('Vous avez perdu 😕 \n' +
-                  'Vous ferez mieux la prochaine fois! 💪')
-            compteurScoreUti = 0;
-            compteurScoreBot = 0;
-            scoreUtilisateur.innerText = compteurScoreUti;
-            scoreOrdi.innerText = compteurScoreBot
-        }
+    //Affiche le résultat quand un des deux joueurs atteint 10
+    if (compteurScoreUti >= 10) {
+        alert('Félicitations!\n' +
+            'Vous avez gagné! 😎 ');
+        compteurScoreUti = 0;
+        compteurScoreBot = 0;
+        scoreUtilisateur.innerText = compteurScoreUti;
+        scoreOrdi.innerText = compteurScoreBot
     }
-
-function compteur(){
-
+    else if (compteurScoreBot >= 10){
+        alert('Vous avez perdu 😕 \n' +
+            'Vous ferez mieux la prochaine fois! 💪')
+        compteurScoreUti = 0;
+        compteurScoreBot = 0;
+        scoreUtilisateur.innerText = compteurScoreUti;
+        scoreOrdi.innerText = compteurScoreBot
+    }
 }
+
 
